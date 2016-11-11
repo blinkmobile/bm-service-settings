440e687533679472003f30f9900acea84d79dc2e
Matt Carroll <matt.carroll139@gmail.com>
2016-11-11 11:28:31 +1100

    SC-32 # Updated route to include version

---
+++ CHANGLOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+## 0.0.1
+
+### Added
+
+-   SC-32: `/v1/settings` route
+
+-   SC-32: Support for `@blinkmobile/server-cli` settings
--- api/v1/settings.js
+++ api/v1/settings.js
@@ -2,7 +2,7 @@
 'use strict'
 
 /* ::
-import type {BmRequest} from '../types.js'
+import type {BmRequest} from '../../types.js'
 */
 
 // Use dotenv to store secrets
@@ -11,8 +11,8 @@ const dotenv = require('dotenv')
 // Configure dotenv to get secrets and set as environment variables
 dotenv.config()
 
-const authenticate = require('../lib/authenticate.js')
-const settings = require('../lib/settings.js')
+const authenticate = require('../../lib/authenticate.js')
+const settings = require('../../lib/settings.js')
 
 module.exports.get = function get (
   request /* : BmRequest */
