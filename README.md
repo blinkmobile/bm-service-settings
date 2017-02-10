f404a4b44847502dea3ee5779be57525d71f3682
Matt Carroll <matt.carroll139@gmail.com>
2017-02-10 13:31:22 +1100

    SS-3 # Updated deps and added flow to tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# bm-service-settings [![AppVeyor Status](https://img.shields.io/appveyor/ci/blinkmobile/bm-service-settings/master.svg)](https://ci.appveyor.com/project/blinkmobile/bm-service-settings) [![Travis CI Status](https://travis-ci.org/blinkmobile/bm-service-settings.svg?branch=master)](https://travis-ci.org/blinkmobile/bm-service-settings)
+# bm-service-settings [![Travis CI Status](https://travis-ci.org/blinkmobile/bm-service-settings.svg?branch=master)](https://travis-ci.org/blinkmobile/bm-service-settings) [![npm](https://img.shields.io/npm/v/bm-service-settings.svg?maxAge=2592000)](https://www.npmjs.com/package/bm-service-settings)
 
 Provides settings for BlinkMobile services.
 
--- test/lib/server-cli.js
+++ test/lib/server-cli.js
@@ -1,25 +1,29 @@
+/* @flow */
 'use strict'
 
 const test = require('ava')
 const Boom = require('boom')
 const proxyquire = require('proxyquire')
 
+const logger = require('../fixtures/logger.js')
+
 const TEST_SUBJECT = '../../lib/server-cli.js'
 
 const PROJECT_NAME = 'valid project name'
 const JSON_API_PROJECT = require('../fixtures/json-api-data.json')
 
+const getTestSubject = (overrides) => {
+  overrides = overrides || {}
+  return proxyquire(TEST_SUBJECT, Object.assign({
+    'request': {
+      get: (url, data, cb) => cb(null, {statusCode: 200}, JSON_API_PROJECT)
+    }
+  }, overrides))
+}
+
 test.beforeEach((t) => {
   process.env.PROJECT_REGISTRY_ORIGIN = 'this is my project registry url'
   process.env.PROJECT_REGISTRY_SECRET = 'this is my project registry secret'
-  t.context.getTestSubject = (overrides) => {
-    overrides = overrides || {}
-    return proxyquire(TEST_SUBJECT, Object.assign({
-      'request': {
-        get: (url, data, cb) => cb(null, {statusCode: 200}, JSON_API_PROJECT)
-      }
-    }, overrides))
-  }
 })
 
 test.afterEach((t) => {
@@ -29,24 +33,24 @@ test.afterEach((t) => {
 
 test('Should reject with Boom error if bmProject is invalid', (t) => {
   t.plan(1)
-  const lib = t.context.getTestSubject()
+  const lib = getTestSubject()
 
-  return lib()
+  return lib(logger)
     .catch((err) => t.deepEqual(err, Boom.badRequest('Invalid \'bmProject\' parameter on query string')))
 })
 
 test('Should pass correct arguments to request.get() and resolve to settings', (t) => {
   t.plan(2)
-  const lib = t.context.getTestSubject({
+  const lib = getTestSubject({
     'request': {
       get: (url, data, cb) => {
-        t.is(url, `${process.env.PROJECT_REGISTRY_ORIGIN}/v1/projects?filter={"attributes.name":"${PROJECT_NAME}"}&include=service`)
+        t.is(url, `${process.env.PROJECT_REGISTRY_ORIGIN || ''}/v1/projects?filter={"attributes.name":"${PROJECT_NAME || ''}"}&include=service`)
         cb(null, {statusCode: 200}, JSON_API_PROJECT)
       }
     }
   })
 
-  return lib(PROJECT_NAME)
+  return lib(logger, PROJECT_NAME)
     .then((settings) => t.deepEqual(settings, {
       bucket: JSON_API_PROJECT.included[0].attributes.awsS3Bucket,
       serviceOrigin: JSON_API_PROJECT.included[0].attributes.origin
@@ -55,36 +59,58 @@ test('Should pass correct arguments to request.get() and resolve to settings', (
 
 test('Should reject with Boom error if request.get() returns an error', (t) => {
   t.plan(1)
-  const lib = t.context.getTestSubject({
+  const lib = getTestSubject({
     'request': {
       get: (url, data, cb) => cb(new Error('test error'))
     }
   })
 
-  return lib(PROJECT_NAME)
+  return lib(logger, PROJECT_NAME)
     .catch((err) => t.deepEqual(err, Boom.wrap(new Error('test error'))))
 })
 
 test('Should reject with correct Boom error if request.get() does not return a statusCode of 200', (t) => {
   t.plan(1)
-  const lib = t.context.getTestSubject({
+  const lib = getTestSubject({
     'request': {
       get: (url, data, cb) => cb(null, {statusCode: 404}, {message: 'missing'})
     }
   })
 
-  return lib(PROJECT_NAME)
+  return lib(logger, PROJECT_NAME)
     .catch((err) => t.deepEqual(err, Boom.notFound('missing')))
 })
 
 test('Should reject with correct not found Boom error if request.get() does not return a single project', (t) => {
   t.plan(1)
-  const lib = t.context.getTestSubject({
+  const lib = getTestSubject({
     'request': {
       get: (url, data, cb) => cb(null, {statusCode: 200}, {data: []})
     }
   })
 
-  return lib(PROJECT_NAME)
+  return lib(logger, PROJECT_NAME)
     .catch((err) => t.deepEqual(err, Boom.notFound(`Could not find project: ${PROJECT_NAME}`)))
+})
+
+test('Should reject with correct not found Boom error if request.get() does not return a service for project', (t) => {
+  t.plan(1)
+  const lib = getTestSubject({
+    'request': {
+      get: (url, data, cb) => cb(null, {statusCode: 200}, {
+        data: [
+          {
+            'id': '10',
+            'type': 'projects',
+            'attributes': {
+              'name': 'bm-service-settings.api.blinkm.io'
+            }
+          }
+        ]
+      })
+    }
+  })
+
+  return lib(logger, PROJECT_NAME)
+    .catch((err) => t.deepEqual(err, Boom.notFound(`Could not find service for project: ${PROJECT_NAME}`)))
 })
